c2558e97b7840ee49697b4dc4a8227d1158f8fbe
Manuel Altalef <maltalef@auror.ar>
2023-04-08 14:14:34 -0300

    packages

--- modules/packages.nix
+++ modules/packages.nix
@@ -8,7 +8,7 @@ in {
   options.modules.packages = { enable = mkEnableOption "packages"; };
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-      ripgrep ffmpeg exa gnupg bat skim nsxiv mpv pass gnupg unzip 7zip slop maim polybar nvtop
+      ripgrep ffmpeg exa gnupg bat skim nsxiv mpv pass gnupg unzip 7zip maim polybar nvtop
     ];
   };
 }
