fbc4d57c5e13e17ad8efa50e30e3512cab566541
Manuel Altalef <maltalef@auror.ar>
2023-04-09 20:07:15 -0300

    storage

---
+++ modules/storage.nix
@@ -0,0 +1,13 @@
+{ pkgs, lib, config, ... }:
+
+with lib;
+let cfg = config.modules.storage;
+
+in {
+  options.modules.storage = { enable = mkEnableOption "storage"; };
+  config = mkIf cfg.enable {
+    home.file."00storage" = {
+	  source = /var/lib/mount/storage;
+	  recursive = true;
+  };
+}
