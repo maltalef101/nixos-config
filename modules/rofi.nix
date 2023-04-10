83895b75ae6e91463f3d393bc84d52eee83d2080
Manuel Altalef <maltalef@auror.ar>
2023-04-09 21:32:35 -0300

    commit

--- modules/rofi.nix
+++ modules/rofi.nix
@@ -6,9 +6,11 @@ let cfg = config.modules.rofi;
 in {
   options.modules.rofi = { enable = mkEnableOption "rofi"; };
   config = mkIf cfg.enable {
-    programs.rofi = {
-  	  enable = true;
-	  theme = "${pkgs.rofi}/share/rofi/themes/gruvbox-dark-hard.rasi";
-	};
+    programs.rofi.enable = true;
+
+
+	home.file.".config/rofi/config.rasi".text = ''
+	  @import "${pkgs.rofi-unwrapped}/share/rofi/themes/gruvbox-dark-hard.rasi"
+	'';
   };
 }
