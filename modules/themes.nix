21fbdee34126ff94562e495c26cb9728bd0e160b
Manuel Altalef <maltalef@auror.ar>
2023-04-14 08:52:00 -0300

    fix

--- modules/themes.nix
+++ modules/themes.nix
@@ -6,15 +6,13 @@ let cfg = config.modules.themes;
 in {
   options.modules.themes = { enable = mkEnableOption "themes"; };
   config = mkIf cfg.enable {
-      home = {
-      packages = with pkgs; [
-        gruvbox-dark-gtk
-      ];
-  
-      gtk = {
-        enable = true;
-        theme.name = "gruvbox-dark-gtk"
-      };
+    home.packages = with pkgs; [
+      gruvbox-dark-gtk
+    ];
+
+    gtk = {
+      enable = true;
+      theme.name = "gruvbox-dark-gtk";
     };
   };
 }
