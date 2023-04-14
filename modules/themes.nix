091c06a565d7daabe85448f03dccd582802f53b9
Manuel Altalef <maltalef@auror.ar>
2023-04-14 08:50:06 -0300

    forgot this

---
+++ modules/themes.nix
@@ -0,0 +1,20 @@
+{ pkgs, lib, config, ... }:
+
+with lib;
+let cfg = config.modules.themes;
+
+in {
+  options.modules.themes = { enable = mkEnableOption "themes"; };
+  config = mkIf cfg.enable {
+      home = {
+      packages = with pkgs; [
+        gruvbox-dark-gtk
+      ];
+  
+      gtk = {
+        enable = true;
+        theme.name = "gruvbox-dark-gtk"
+      };
+    };
+  };
+}
